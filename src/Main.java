ff8761bf63572552099cb3631df8913af5d7ea12
Yorick Hennink <yorick.hennink@haskoning.com>
2025-07-28 15:24:08 +0200

    versie 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Which number would you like to check?");
+        int number = scanner.nextInt();
+        if (number %2 ==0){
+            System.out.print("Number is even");
+        } else {
+            System.out.print("Number is uneven");
+        }
+    }}
